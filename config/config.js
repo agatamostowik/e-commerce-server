7f2efdfdf16ec3a143a94e4832bc4f7dd2472689
Agata Mostowik <agatamostowik@gmail.com>
2023-01-16 20:36:04 +0100

    sequeliuze

---
+++ config/config.js
@@ -0,0 +1,27 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+module.exports = {
+  development: {
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    dialect: "postgres",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+};
--- src/app.js
+++ src/app.js
@@ -1,9 +1,32 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const db = async () => {
+  const sequelize = new Sequelize({
+    dialect: "postgres",
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+  });
+
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
 
 export const initApp = () => {
   const app = express();
+
+  db();
 
   // Global middlewares
   app.use(cors());
